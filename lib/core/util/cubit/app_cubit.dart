b267b1a060dfdcce2d3b50daa6f48b1f6588d51e
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-07-16 13:24:15 +0200

    Add Tdd Files.

---
+++ lib/core/util/cubit/app_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:clean_arctect/core/util/cubit/app_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppCubit extends Cubit<AppState> {
+  AppCubit() : super(InitialState());
+}
